c319c6473b78db761529aa9b3e33eb1378fbc1c8
Jeremy Storm <storm.jeremy@gmail.com>
2016-07-20 21:29:48 +0200

    Cleaning up db-data

--- src/clj/lang_site/db/db_data.clj
+++ src/clj/lang_site/db/db_data.clj
@@ -4,6 +4,9 @@
             [clojure.string :as str :only split]
             [clojure.core.async :as async :refer [<! >! >!! put! take!]]))
 
+(defn split-by-tab [s]
+  (str/split s #"\t"))
+
 (def events (async/chan 1000000))
 
 (def event-bus (async/chan 1000))
@@ -20,9 +23,15 @@
 
 (def uri "datomic:couchbase://localhost:4334/datomic/lang-site/?password=password")
 
-(d/create-database uri)
+(defn create-lang-db []
+  (d/create-database uri))
 
 (def conn (d/connect uri))
+
+(async/go
+  (while true
+    (let [tx (<! events)]
+      (ingest conn tx))))
 
 (def rdr-s (clojure.java.io/reader "resources/data/sentences.csv"))
 
@@ -30,19 +39,33 @@
 
 (def schema-tx (read-string (slurp "resources/data/lang-site-schema.edn")))
 
-@(d/transact conn schema-tx)
+(defn load-schema [conn]
+  @(d/transact conn schema-tx))
+
+(comment (d/transact
+          conn
+          (take 100000
+                (map :tx
+                     (map links-template
+                          (keep identity
+                                (map
+                                 (comp #(find-translation-pair (d/db conn) %)
+                                       split-by-tab)
+                                 (line-seq rdr-l))))))))
+
+#_ (defn read-links
+     (let f (comp #(map tx %)
+                  #(map links-template %)
+                  #(keep identity %)
+                  #(map
+                    (comp)))))
 
 (defn find-translation-pair [db [s-id t-id]]
   (d/q '[:find [?e1 ?e2]
          :in $ ?sentence-id ?translation-id
          :where [?e1 :sentence/id ?sentence-id]
                 [?e2 :sentence/id ?translation-id]]
-       db s-id t-id))
-
-#_(defn links-template [[sentence-id translation-id]]
-  {:db/id #db/id[:db.part/user]
-   :sentence/id sentence-id
-   :sentence/translation translation-id})
+       db  (read-string s-id) (read-string t-id)))
 
 (defn links-template [[sentence-id translation-id]]
   {:type :link
@@ -50,14 +73,18 @@
    {:db/id #db/id[:db.part/user]
     :translation/group [sentence-id translation-id]}})
 
-(defn link-to-datomic [line]
-  (let [vals (str/split line #"\t")
-        ids  (find-translation-pair (d/db conn) vals)]
-    (if (= (count (get-in ids [:tx :translation/group])) 2)
+(defn link-to-datomic [db line]
+  (let [vals (split-by-tab line)
+        ids  (find-translation-pair db vals)]
+    (if (= (count ids) 2)
       (->> ids
            (links-template)
            (put! events)))))
 
+(defn get-links [db]
+  (keep identity
+        (map (comp #(find-translation-pair db %) split-by-tab)
+             (line-seq rdr-l))))
 
 (defn sentence-template [[id lang text]]
   {:type :sentence
@@ -70,46 +97,30 @@
     :sentence/text text}})
 
 (defn sentence-to-datomic [sent]
-  (as-> sent s
-    (str/split s #"\t")
-    (sentence-template s)
-    (put! events s)))
+  (->> sent
+       (split-by-tab)
+       (sentence-template)
+       (put! events)))
 
-(defmulti ingest
+(defmulti validate-tx
   "Ingest a transaction into Datomic DB"
   :type)
 
-(defmethod ingest :sentence [{{lang :sentence/language :as tx} :tx}]
+(defmethod validate-tx :sentence [conn {{lang :sentence/language :as tx} :tx}]
   (if (or (= lang :sentence.language/eng) ; only support tur and english
           (= lang :sentence.language/tur))
-    (d/transact conn [tx])))
+    tx))
 
-(defmethod ingest :link [{{group :translation/group :as tx} :tx}]
+(defmethod validate :link [conn {{group :translation/group :as tx} :tx}]
   (if-not (some true?
                 (d/pull-many (d/db conn) [:translation_/group] tx))
-    (d/transact conn [tx])))
+    tx))
 
-(async/go
-  (while true
-    (let [tx (<! events)]
-      (ingest tx))))
-
-#_(async/go
-  (while true
-    (let [tx (<! events)]
-      (if (or (= (:sentence/language tx) :sentence.language/eng)
-              (= (:sentence/language tx) :sentence.language/tur))
-        (d/transact conn [tx])))))
-
-#_(async/go
-  (while true
-    (let [tx (<! events)]
-      (if (:translation/group tx)
-        (if-not (some true?
-                      (d/pull-many (d/db conn)
-                                   [:translation_/group]
-                                   (:translation/group tx)))
-          (d/transact conn [tx]))))))
+(defn process-transactions [conn]
+  (async/go
+    (while true
+      (let [tx (<! events)]
+        (d/transact conn (validate-tx tx))))))
 
 (defn excise-template [tx-id]
   [{:db/id #db/id[:db.part/user]
@@ -130,3 +141,31 @@
          :in $ ?search
          :where [(fulltext $ :sentence/text ?search) [[?e]]]]
        db search))
+
+(defn find-links [db]
+  (d/q '[:find ?a
+         :where [_ :translation/group ?a]]
+       db))
+
+(defn find-links-pull [db]
+  (d/q '[:find (pull ?a [*])
+         :where [_ :translation/group ?a]]
+       db))
+
+(defn find-fulltext [db search-string]
+  (d/q '[:find (pull ?e [*]) .
+         :in $ ?search
+         :where [(fulltext $ :sentence/text ?search) [[?e]]]]
+       db search-string))
+
+;; Sample ENG<->TUR pair
+;; English 3898641 17592187516140, Turkish 3949090, 17592187570252
+;; ["3898641" "3949090"] "3898641\t3949090"
+;; 17592186156439, 170564; 17592186168085, 243919
+;; "170564\t243919"
+
+(defn pull-by-sentence [db sentence]
+  (d/q '[:find (pull ?e [*]) .
+         :in $ ?sentence
+         :where [?e :sentence/text ?sentence]]
+       db sentence))
